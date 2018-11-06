3090253edde903638aaf81b8de8995c193febfd4
jped <jonathanped@gmail.com>
2018-11-06 01:11:26 -0500

    added process script and readme alongside with it

---
+++ code/README.md
@@ -0,0 +1,12 @@
+#Usage
+
+
+
+## Preprocess script
+This script takes in a data file and saves a pickle with the token dictionary.
+It also filters out words below a certain threshold. :x
+
+Usage: 
+```
+ python process.py <FILE_NAME_OF_DATA> <DESIRED_PICKLE_FILENAME> <THRESHOLD>
+```
---
+++ code/process.py
@@ -0,0 +1,28 @@
+import sys
+import pickle
+
+def process_text(text_file,pickle_name,THRESH):
+    frequency_dict = {}
+    word_dict= {}
+    count = 0
+    with open(text_file, "r") as text: 
+        for line in text:
+            for word in line.split(): 
+                if word in word_dict:
+                    frequency_dict[word] +=1
+                else: 
+                    word_dict[word] = count
+                    count+=1
+                    frequency_dict[word] = 1
+    word_dict = {word:word_dict[word] for word in word_dict if frequency_dict[word]>THRESH}     
+    pickle.dump(word_dict, open(pickle_name, "wb"))
+    #print(word_dict) 
+
+if __name__ == "__main__":
+    #Usage, pass in the text file, then the name of the pickle file for the dictionary, and then the thresh for word frequency
+    text_file = sys.argv[1]
+    pickle_name = sys.argv[2]
+    THRESH = int(sys.argv[3])
+    print("Starting to process")
+    process_text(text_file, pickle_name, THRESH)
+    print("Done Processing")
