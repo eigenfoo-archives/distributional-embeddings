965fbdfb7f5d532324a76ba46a2a1c5c5af9eeeb
George Ho <georgeho1618@gmail.com>
2018-11-11 14:25:33 -0500

    BLD: building... need to implement train and reg now

--- src/gaussian.py
+++ src/gaussian.py
@@ -8,9 +8,6 @@ import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
 
-# TODO read data in... or something
-# data = tf.placeholder(tf.float32, shape=[], name='data')
-
 try:
     VOCAB_SIZE = int(sys.argv[1])
     EMBED_DIM = int(sys.argv[2])
@@ -35,7 +32,31 @@ iterator = dataset.make_one_shot_iterator()
 next_line = iterator.get_next()  # Usage: sess.run(next_line)
 
 # Initialize embeddings
-mu_center = tf.get_variable('mu_center', [VOCAB_SIZE, EMBED_DIM],
-                            tf.float32, tf.random_normal_initializer)
-sigma_center = tf.get_variable('sigma_center', [VOCAB_SIZE, EMBED_DIM],
-                               tf.float32, tf.ones_initializer)
+mu = tf.get_variable('mu', [VOCAB_SIZE, EMBED_DIM],
+                     tf.float32, tf.random_normal_initializer)
+sigma = tf.get_variable('sigma', [VOCAB_SIZE, EMBED_DIM],
+                        tf.float32, tf.ones_initializer)
+
+while True:
+    try:
+        line = sess.run(next_line)
+    except tf.errors.OutOfRangeError:
+        print('End of dataset.')
+        break
+
+    # FIXME need to figure this out with Jonny. There are two parts to this:
+    # getting the samples, and then converting them to unique integer ids.
+    center_id, context_ids, negative_ids = next_sample()
+
+    # Look up embeddings
+    # FIXME we should perform only two embedding_lookups, and index.
+    center_mu = tf.nn.embedding_lookup(mu, center_id)
+    center_sigma = tf.nn.embedding_lookup(sigma, center_id)
+    context_mu = tf.nn.embedding_lookup(mu, context_ids)
+    context_sigma = tf.nn.embedding_lookup(sigma, context_ids)
+    negative_mu = tf.nn.embedding_lookup(mu, negative_ids)
+    negative_sigma = tf.nn.embedding_lookup(sigma, negative_ids)
+
+    # TODO Train
+
+    # TODO Regularize (e.g. eigenvalues of covariance matrix)
