35d4ddc5a21a74486322e0374ee8538db76ec605
George Ho <georgeho1618@gmail.com>
2018-11-11 14:05:42 -0500

    BLD: using tf.data API

--- src/gaussian.py
+++ src/gaussian.py
@@ -3,6 +3,7 @@ Implementation of Word Representations via Gaussian Embedding.
     https://arxiv.org/abs/1412.6623
 '''
 
+import sys
 import numpy as np
 import tensorflow as tf
 from tqdm import tqdm
@@ -10,19 +11,31 @@ from tqdm import tqdm
 # TODO read data in... or something
 # data = tf.placeholder(tf.float32, shape=[], name='data')
 
-EMBED_DIM = 20
-VOCAB_SIZE = 10000  # FIXME compute this from data shape
-NUM_ITER = 100000
+try:
+    VOCAB_SIZE = int(sys.argv[1])
+    EMBED_DIM = int(sys.argv[2])
+    NUM_EPOCHS = int(sys.argv[3])
+except IndexError:
+    print('\nUsage:\n\tpython gaussian.py VOCAB_SIZE EMBED_DIM NUM_EPOCHS\n')
+    sys.exit()
+
+# Point Tensorflow to data file
+filenames = ['../data/data.txt']
+dataset = tf.data.Dataset.from_tensor_slices(filenames)
+
+# Filter out empty lines
+dataset = dataset.flat_map(
+    lambda filename: (tf.data.TextLineDataset(filename)
+                        .filter(lambda line: tf.not_equal(line, '')))
+    )
+
+# Batch size of line 1 each. Repeat as many times as we have epochs.
+dataset = dataset.batch(1).repeat(NUM_EPOCHS)
+iterator = dataset.make_one_shot_iterator()
+next_line = iterator.get_next()  # Usage: sess.run(next_line)
 
 # Initialize embeddings
 mu_center = tf.get_variable('mu_center', [VOCAB_SIZE, EMBED_DIM],
                             tf.float32, tf.random_normal_initializer)
-mu_context = tf.get_variable('mu_context', [VOCAB_SIZE, EMBED_DIM],
-                             tf.float32, tf.random_normal_initializer)
 sigma_center = tf.get_variable('sigma_center', [VOCAB_SIZE, EMBED_DIM],
                                tf.float32, tf.ones_initializer)
-sigma_context = tf.get_variable('sigma_center', [VOCAB_SIZE, EMBED_DIM],
-                                tf.float32, tf.ones_initializer)
-
-for it in range(NUM_ITERS):
-    pass
