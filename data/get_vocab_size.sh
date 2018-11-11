f979ab4fdd0076cbe075ab3af91b4cd901fc0285
George Ho <georgeho1618@gmail.com>
2018-11-11 13:53:25 -0500

    DOC: document shell script

--- data/get_vocab_size.sh
+++ data/get_vocab_size.sh
@@ -1,3 +1,3 @@
-# Expects a data.txt
+# Counts the number of unique words in data.txt
 # From https://stackoverflow.com/a/34377954
 tr ':;,?!\"' ' ' < data.txt | tr -s ' ' '\n' | awk '!a[$0]++{c++} END{print c}'
