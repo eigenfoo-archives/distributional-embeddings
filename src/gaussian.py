e35a7d9c2d67da00f25703f550402cc9d5310d59
George Ho <georgeho1618@gmail.com>
2018-11-13 01:08:36 -0500

    MAINT: clean up args, doc better

--- src/gaussian.py
+++ src/gaussian.py
@@ -18,17 +18,15 @@ parser.add_argument('vocab_size', type=int,
                     help='Number of unique tokens in the vocabulary.')
 parser.add_argument('embed_dim', type=int,
                     help='Dimensionality of the embedding space.')
-parser.add_argument('context_size', type=int,
-                    help='Number of tokens to consider in context.')
 parser.add_argument('margin', type=float, nargs='?', default=1.0,
                     help='Margin in max-margin loss. Defaults to 1.')
 parser.add_argument('num_epochs', type=int, nargs='?', default=1,
                     help='Number of epochs. Defaults to 1.')
-parser.add_argument('clip_norm', type=float, nargs='?', default=20.0,
+parser.add_argument('C', type=float, nargs='?', default=20.0,
                     help='Maximum L2 norm of mu. Defaults to 20.')
-parser.add_argument('minimum', type=float, nargs='?', default=1e-3,
+parser.add_argument('m', type=float, nargs='?', default=1e-3,
                     help='Minimum covariance eigenvalue. Defaults to 1e-3.')
-parser.add_argument('maximum', type=float, nargs='?', default=1e3,
+parser.add_argument('M', type=float, nargs='?', default=1e3,
                     help='Maximum covariance eigenvalue. Defaults to 1e3.')
 
 args = parser.parse_args()
@@ -73,25 +71,27 @@ loss = tf.reduce_mean(max_margins)
 # Minimize loss
 train_step = tf.train.AdamOptimizer().minimize(loss)
 
-# Begin training
+# Training
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-with open(args.data_file, 'r') as data_file:
-    for line in tqdm(data_file.readlines()):
-        # Evaluate string as python literal and convert to numpy array
-        context_ids_, negative_ids_, center_id_ = literal_eval(line.strip())
-        context_ids_, negative_ids_, center_id_ = \
-            map(np.array, [context_ids_, negative_ids_, center_id_])
+for _ in range(arg.num_epochs):
+    with open(args.data_file, 'r') as data_file:
+        for line in tqdm(data_file.readlines()):
+            # Evaluate string as python literal and convert to numpy array
+            context_ids_, negative_ids_, center_id_ = \
+                literal_eval(line.strip())
+            context_ids_, negative_ids_, center_id_ = \
+                map(np.array, [context_ids_, negative_ids_, center_id_])
 
-        # Train
-        sess.run(train_step, feed_dict={center_id: center_id_,
-                                        context_ids: context_ids_,
-                                        negative_ids: negative_ids_})
+            # Update
+            sess.run(train_step, feed_dict={center_id: center_id_,
+                                            context_ids: context_ids_,
+                                            negative_ids: negative_ids_})
 
-        # Regularize means and covariance eigenvalues
-        mu = tf.clip_by_norm(mu, args.clip_norm)
-        sigma = tf.maximum(args.minimum, tf.minimum(args.maximum, sigma))
+            # Regularize means and covariance eigenvalues
+            mu = tf.clip_by_norm(mu, args.C)
+            sigma = tf.maximum(args.m, tf.minimum(args.M, sigma))
 
 # Save embedding parameters as .npy files
 mu_np = mu.eval(session=sess)
