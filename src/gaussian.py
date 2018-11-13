bc4f1499bcd364ef203d29d49b88afbefdb0af32
George Ho <georgeho1618@gmail.com>
2018-11-13 00:56:30 -0500

    MAINT: use argparse

--- src/gaussian.py
+++ src/gaussian.py
@@ -3,28 +3,35 @@ Implementation of Word Representations via Gaussian Embedding.
     https://arxiv.org/abs/1412.6623
 '''
 
-import sys
+import argparse
 import numpy as np
 import tensorflow as tf
 from ast import literal_eval
 from tqdm import tqdm
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Train Gaussian embeddings.')
 
-try:
-    VOCAB_SIZE = int(sys.argv[1])
-    EMBED_DIM = int(sys.argv[2])
-    CONTEXT_SIZE = int(sys.argv[3])  # Same as number of negative samples!
-    MARGIN = float(sys.argv[4])
-    NUM_EPOCHS = int(sys.argv[5])
-    CLIP_NORM = float(sys.argv[6])
-    MINIMUM = float(sys.argv[7])
-    MAXIMUM = float(sys.argv[8])
-except IndexError:
-    msg = ('\nUsage:\n\tpython gaussian.py VOCAB_SIZE EMBED_DIM '
-           'CONTEXT_SIZE MARGIN NUM_EPOCHS CLIP_NORM MINIMUM MAXIMUM\n')
-    print(msg)
-    sys.exit()
+parser.add_argument('data_file', type=str,
+                    help='Name of data file.')
+parser.add_argument('vocab_size', type=int,
+                    help='Number of unique tokens in the vocabulary.')
+parser.add_argument('embed_dim', type=int,
+                    help='Dimensionality of the embedding space.')
+parser.add_argument('context_size', type=int,
+                    help='Number of tokens to consider in context.')
+parser.add_argument('margin', type=float, nargs='?', default=1.0,
+                    help='Margin in max-margin loss. Defaults to 1.')
+parser.add_argument('num_epochs', type=int, nargs='?', default=1,
+                    help='Number of epochs. Defaults to 1.')
+parser.add_argument('clip_norm', type=float, nargs='?', default=20.0,
+                    help='Maximum L2 norm of mu. Defaults to 20.')
+parser.add_argument('minimum', type=float, nargs='?', default=1e-3,
+                    help='Minimum covariance eigenvalue. Defaults to 1e-3.')
+parser.add_argument('maximum', type=float, nargs='?', default=1e3,
+                    help='Maximum covariance eigenvalue. Defaults to 1e3.')
 
+args = parser.parse_args()
 
 # FIXME (George) ensure that context_ids and negative_ids have the same shape
 center_id = tf.placeholder(tf.int32, [])
@@ -32,9 +39,9 @@ context_ids = tf.placeholder(tf.int32, [None])
 negative_ids = tf.placeholder(tf.int32, [None])
 
 # Initialize embeddings
-mu = tf.get_variable('mu', [VOCAB_SIZE, EMBED_DIM],
+mu = tf.get_variable('mu', [args.vocab_size, args.embed_dim],
                      tf.float32, tf.random_normal_initializer)
-sigma = tf.get_variable('sigma', [VOCAB_SIZE, EMBED_DIM],
+sigma = tf.get_variable('sigma', [args.vocab_size, args.embed_dim],
                         tf.float32, tf.ones_initializer)
 
 # Look up embeddings
@@ -46,28 +53,31 @@ context_sigmas = tf.nn.embedding_lookup(sigma, context_ids)
 negative_mus = tf.nn.embedding_lookup(mu, negative_ids)
 negative_sigmas = tf.nn.embedding_lookup(sigma, negative_ids)
 
-# Expected likelihood
-coeff_pos = \
-    1 / ((2*np.pi)**EMBED_DIM * tf.reduce_prod(center_sigma + context_sigmas))
+# Compute similarity (i.e. expected likelihood), max margin and loss
+coeff_pos = 1 / ((2*np.pi)**args.embed_dim
+                 * tf.reduce_prod(center_sigma + context_sigmas))
 quadform_pos = \
     (center_mu - context_mus)**2 / (center_sigma + context_sigmas)**2
 positive_energies = coeff_pos * tf.exp(-0.5 * quadform_pos)
 
-coeff_neg = \
-    1 / ((2*np.pi)**EMBED_DIM * tf.reduce_prod(center_sigma + negative_sigmas))
+coeff_neg = 1 / ((2*np.pi)**args.embed_dim
+                 * tf.reduce_prod(center_sigma + negative_sigmas))
 quadform_neg = \
     (center_mu - negative_mus)**2 / (center_sigma + negative_sigmas)**2
 negative_energies = coeff_neg * tf.exp(-0.5 * quadform_neg)
 
-max_margins = tf.maximum(0.0, MARGIN - positive_energies + negative_energies)
+max_margins = tf.maximum(0.0,
+                         args.margin - positive_energies + negative_energies)
 loss = tf.reduce_mean(max_margins)
 
+# Minimize loss
 train_step = tf.train.AdamOptimizer().minimize(loss)
 
+# Begin training
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-with open('sample_data.txt', 'r') as data_file:
+with open(args.data_file, 'r') as data_file:
     for line in tqdm(data_file.readlines()):
         # Evaluate string as python literal and convert to numpy array
         context_ids_, negative_ids_, center_id_ = literal_eval(line.strip())
@@ -80,8 +90,8 @@ with open('sample_data.txt', 'r') as data_file:
                                         negative_ids: negative_ids_})
 
         # Regularize means and covariance eigenvalues
-        mu = tf.clip_by_norm(mu, CLIP_NORM)
-        sigma = tf.maximum(MINIMUM, tf.minimum(MAXIMUM, sigma))
+        mu = tf.clip_by_norm(mu, args.clip_norm)
+        sigma = tf.maximum(args.minimum, tf.minimum(args.maximum, sigma))
 
 # Save embedding parameters as .npy files
 mu_np = mu.eval(session=sess)
